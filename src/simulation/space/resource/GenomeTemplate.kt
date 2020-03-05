d91fc0131dbd182866166f5f326f3ca923774b68
ShMP <matveyshnytkin@gmail.com>
2020-03-05 06:26:02 +0300

    Changed isTemplate flag to a new Genome Template class.

---
+++ src/simulation/space/resource/GenomeTemplate.kt
@@ -0,0 +1,11 @@
+package simulation.space.resource
+
+class GenomeTemplate internal constructor(genome: Genome?) : Genome(genome) {
+    fun getInstantiatedGenome(templateLegacy: ResourceCore): Genome {
+        val genome = Genome(this)
+        genome.deathTime = templateLegacy.deathTime
+        genome.templateLegacy = templateLegacy
+        primaryMaterial = templateLegacy.genome.primaryMaterial
+        return genome
+    }
+}
