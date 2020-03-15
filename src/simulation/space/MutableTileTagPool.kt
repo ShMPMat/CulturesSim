a85d8fe9226513523cc75bf92d18ef6ddd74c844
ShMP <matveyshnytkin@gmail.com>
2020-03-15 20:42:05 +0300

    One more method.

--- src/simulation/space/MutableTileTagPool.kt
+++ src/simulation/space/MutableTileTagPool.kt
@@ -5,5 +5,7 @@ class MutableTileTagPool(private val tags: MutableSet<TileTag> = mutableSetOf())
 
     fun getByType(type: String) = tags.filter { it.type == type }
 
+    fun add(tag: TileTag) = tags.add(tag)
+
     fun remove(tag: TileTag) = tags.remove(tag)
 }
