ced755d4a2ac65c066d8800d756d107be345aedf
ShMP <matveyshnytkin@gmail.com>
2020-03-15 20:30:21 +0300

    Added a small method to the MutableTileTagPool.

--- src/simulation/space/MutableTileTagPool.kt
+++ src/simulation/space/MutableTileTagPool.kt
@@ -3,5 +3,7 @@ package simulation.space
 class MutableTileTagPool(private val tags: MutableSet<TileTag> = mutableSetOf()) {
     fun contains(tag: TileTag) = tags.contains(tag)
 
+    fun getByType(type: String) = tags.filter { it.type == type }
+
     fun remove(tag: TileTag) = tags.remove(tag)
 }
