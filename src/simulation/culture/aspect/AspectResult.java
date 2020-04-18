f1223f47fedcc7d2add6268f35e421ceec78f005
ShMP <matveyshnytkin@gmail.com>
2020-04-18 19:02:45 +0300

    Added needed Resources to the AspectResult, but they isn't used now.

--- src/simulation/culture/aspect/AspectResult.java
+++ src/simulation/culture/aspect/AspectResult.java
@@ -1,28 +1,39 @@
 package simulation.culture.aspect;
 
+import kotlin.Pair;
 import simulation.space.resource.MutableResourcePack;
 import simulation.space.resource.tag.ResourceTag;
+import simulation.space.resource.tag.labeler.ResourceLabeler;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class AspectResult {
     public boolean isFinished;
+    public List<Pair<ResourceLabeler, Integer>> neededResources;
     public MutableResourcePack resources;
     public ResultNode node;
 
-    public AspectResult(boolean isFinished, MutableResourcePack resources, ResultNode node) {
+    public AspectResult(
+            boolean isFinished,
+            List<Pair<ResourceLabeler, Integer>> neededResources,
+            MutableResourcePack resources,
+            ResultNode node
+    ) {
         this.isFinished = isFinished;
+        this.neededResources = neededResources;
         this.resources = resources;
         this.node = node;
     }
 
     public AspectResult(boolean isFinished, ResultNode node) {
-        this(isFinished, new MutableResourcePack(), node);
+        this(isFinished, new ArrayList<>(), new MutableResourcePack(), node);
     }
 
     public AspectResult(MutableResourcePack resources, ResultNode node) {
-        this(true, resources, node);
+        this(true, new ArrayList<>(), resources, node);
     }
 
     public static class ResultNode {
