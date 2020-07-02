b09049bcb139332eb42b62a8a2141cca31eb0588
ShMP <matveyshnytkin@gmail.com>
2020-07-02 12:08:36 +0300

    Moved BaseStratum to a new file.

--- src/simulation/culture/group/stratum/Stratum.kt
+++ src/simulation/culture/group/stratum/Stratum.kt
@@ -1,15 +1,9 @@
 package simulation.culture.group.stratum
 
-import shmp.random.randomElement
-import simulation.Controller.session
 import simulation.culture.group.centers.Group
 import simulation.culture.group.place.StaticPlace
-import simulation.culture.group.request.resourceToRequest
 import simulation.space.Territory
-import simulation.space.resource.Resource
 import simulation.space.resource.container.MutableResourcePack
-import simulation.space.tile.Tile
-import simulation.space.tile.TileTag
 
 interface Stratum {
     val population: Int
@@ -32,51 +26,6 @@ interface Stratum {
     fun finishUpdate(group: Group)
 
     fun die()
-}
-
-
-abstract class BaseStratum(tile: Tile, protected val name: String) : Stratum {
-    override val ego = Ego(tile, name)
-
-
-    protected val innerPlaces = mutableListOf<StaticPlace>()
-    override val places: List<StaticPlace>
-        get() = innerPlaces
-
-    override fun update(accessibleResources: MutableResourcePack, accessibleTerritory: Territory, group: Group) {
-        places.forEach { place ->
-            val lacking = place.getLacking()
-            val gotResources = lacking
-                    .map { resourceToRequest(it, group, it.amount) }
-                    .flatMap { group.populationCenter.executeRequest(it).pack.resources }
-            place.addResources(gotResources)
-        }
-    }
-
-    protected fun addEnhancement(resource: Resource, group: Group) {
-        val goodPlaces = innerPlaces.filter { resource.genome.isAcceptable(it.tile) }
-        var place: StaticPlace? = null
-
-        if (goodPlaces.isEmpty()) {
-            val goodTiles = group.territoryCenter.territory
-                    .getTiles { resource.genome.isAcceptable(it) }
-            if (goodTiles.isNotEmpty()) {
-                val tagType = "(Stratum $name of ${group.name})"
-                place = StaticPlace(
-                        randomElement(goodTiles, session.random),
-                        TileTag(tagType + "_" + innerPlaces.size, tagType)
-                )
-                innerPlaces.add(place)
-            }
-        } else
-            place = randomElement(innerPlaces, session.random)
-
-        if (place == null) return
-
-        place.addResource(resource)
-    }
-
-    override fun toString() = "\nEgo resources: ${ego.place.current.owned}"
 }
 
 
