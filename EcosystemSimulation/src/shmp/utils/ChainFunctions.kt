72573e2bce185264a5050c05bd65693f745d5b23
ShMP <matveyshnytkin@gmail.com>
2021-10-07 20:17:02 +0300

    Flung all utils to a separate project.

--- EcosystemSimulation/src/shmp/utils/ChainFunctions.kt
+++
@@ -1,4 +0,0 @@
-package shmp.utils
-
-
-fun <E> Collection<E>.without(element: E) = filter { it != element }
--- EcosystemSimulation/src/shmp/utils/labeler/EqualityLabeler.kt
+++
@@ -1,8 +0,0 @@
-package shmp.utils.labeler
-
-
-class EqualityLabeler<T>(val reference: T) : AbstractLabeler<T>() {
-    override fun matches(sample: T) = reference == sample
-
-    override fun explanation() = "equals to $reference"
-}
