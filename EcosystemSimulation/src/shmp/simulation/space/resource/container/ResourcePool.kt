bfc1fa1ec59298864a091a1d0422afb3559bc937
ShMP <matveyshnytkin@gmail.com>
2023-02-22 16:31:07 +0300

    Refactor ResourcePool

--- EcosystemSimulation/src/shmp/simulation/space/resource/container/ResourcePool.kt
+++ EcosystemSimulation/src/shmp/simulation/space/resource/container/ResourcePool.kt
@@ -4,24 +4,29 @@ import shmp.simulation.space.resource.Resource
 import shmp.simulation.space.resource.ResourceIdeal
 
 
-class ResourcePool(val resources: List<ResourceIdeal>) {
-    private val baseNameMap: Map<String, ResourceIdeal> = resources
-            .map { it.baseName to it }
-            .toMap()
+class ResourcePool(private val resources: List<ResourceIdeal>) {
+    private val baseNameMap: Map<String, ResourceIdeal> =
+            resources.associateBy { it.baseName }
 
-    val simpleNameMap = resources.groupBy { it.simpleName }
+    val simpleNameMap: Map<String, List<ResourceIdeal>> =
+            resources.groupBy { it.simpleName }
 
-    fun get(predicate: (Resource) -> Boolean) = resources.firstOrNull(predicate)?.copy()
+    fun get(predicate: (Resource) -> Boolean): Resource? =
+            resources.firstOrNull(predicate)?.copy()
 
-    fun getAll(predicate: (Resource) -> Boolean) = resources.filter(predicate).map { it.copy() }
+    fun getAll(predicate: (Resource) -> Boolean): List<Resource> =
+            resources.filter(predicate).map { it.copy() }
 
-    fun getBaseNameOrNull(name: String) = baseNameMap[name]?.copy()
+    fun getBaseNameOrNull(name: String): Resource? =
+            baseNameMap[name]?.copy()
 
-    fun getBaseName(name: String) = getBaseNameOrNull(name)
-            ?: throw NoSuchElementException("No resource with name $name")
+    fun getBaseName(name: String): Resource =
+            getBaseNameOrNull(name)
+                    ?: throw NoSuchElementException("No resource with name $name")
 
-    fun getSimpleName(name: String) = get { it.simpleName == name }
-            ?: throw NoSuchElementException("No resource with name $name")
+    fun getSimpleName(name: String): Resource =
+            get { it.simpleName == name }
+                    ?: throw NoSuchElementException("No resource with name $name")
 
     val all: List<Resource> = resources
 }
