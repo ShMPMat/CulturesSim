ee1c62afd30522a7d13cc2a098364ca27930ab50
ShMP <matveyshnytkin@gmail.com>
2023-07-18 10:02:02 +0300

    Fix TagTransformer not updating already existing tag

--- EcosystemSimulation/src/io/tashtabash/simulation/space/resource/transformer/TagTransformer.kt
+++ EcosystemSimulation/src/io/tashtabash/simulation/space/resource/transformer/TagTransformer.kt
@@ -7,7 +7,7 @@ import io.tashtabash.simulation.space.resource.tag.ResourceTag
 
 class TagTransformer(val tag: ResourceTag) : ResourceTransformer {
     override fun transform(resource: Resource): Resource {
-        val genome = resource.genome.copy(tags = resource.genome.tags + listOf(tag))
+        val genome = resource.genome.copy(tags = resource.genome.tags - tag + tag)
         val core = resource.core.copy(genome = genome)
         return Resource(core, resource.amount).replaceRecursiveLinks(resource)
     }
