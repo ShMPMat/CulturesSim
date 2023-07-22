d7167a1aa19dbdf5b9c7c75c130f422c5a747dae
ShMP <matveyshnytkin@gmail.com>
2023-07-22 08:33:57 +0300

    Fix compilation error with new EventTypes

--- EcosystemSimulation/src/io/tashtabash/simulation/event/Event.kt
+++ EcosystemSimulation/src/io/tashtabash/simulation/event/Event.kt
@@ -15,12 +15,15 @@ enum class Type(val colourString: String) {
     Death(("\u001B[31m")),
     ResourceDeath(("\u001B[31m")),
     AspectGaining("\u001B[32m"),
+    CultureAspectGaining("\u001B[32m"),
     TileAcquisition("\u001B[32m"),
     DisbandResources("\u001B[32m"),
 
     GroupInteraction("\u001B[35m"),
     Conflict("\u001B[31m"),
     Cooperation("\u001B[32m"),
+
+    Cataclysm(("\u001B[31m")),
 
     Creation("\u001B[34m"),
     Move("\u001B[33m"),
