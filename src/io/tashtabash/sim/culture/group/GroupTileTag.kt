cd163daec0e1d725892ba71c367d4a07837ef906
smp <matvey.shnytkin@soft-werke.com>
2023-10-03 09:24:07 +0400

    Fix GroupTileTag formatting

--- src/io/tashtabash/sim/culture/group/GroupTileTag.kt
+++ src/io/tashtabash/sim/culture/group/GroupTileTag.kt
@@ -4,6 +4,7 @@ import io.tashtabash.sim.culture.group.centers.Group
 import io.tashtabash.sim.space.tile.Tile
 import io.tashtabash.sim.space.tile.TileTag
 
+
 const val GROUP_TAG_TYPE = "Group"
 
 class GroupTileTag(val group: Group) : TileTag(group.name, GROUP_TAG_TYPE)
