589ad6cbda7d9ae8a838dc90ec4f03325e18bf70
ShMPMat <matveyshnytkin@gmail.com>
2026-02-25 09:15:08 +0400

    Move ResolveResourceNeedB

--- src/io/tashtabash/sim/culture/group/process/behaviour/AspectBehaviours.kt
+++ src/io/tashtabash/sim/culture/group/process/behaviour/AspectBehaviours.kt
@@ -1,12 +1,33 @@
 package io.tashtabash.sim.culture.group.process.behaviour
 
+import io.tashtabash.random.singleton.chanceOfNot
 import io.tashtabash.random.singleton.randomElementOrNull
 import io.tashtabash.sim.CulturesController.Companion.session
 import io.tashtabash.sim.culture.group.centers.Group
 import io.tashtabash.sim.culture.group.process.ProcessResult
+import io.tashtabash.sim.culture.group.process.emptyProcessResult
 import io.tashtabash.sim.event.Change
 import io.tashtabash.sim.event.of
 
+
+object ResolveResourceNeedB : PlanBehaviour() {
+    override fun run(group: Group): ProcessResult {
+        val need = group.resourceCenter.mostImportantNeed
+            ?: return emptyProcessResult
+
+        (1 - 1.0 / (1 + need.second.importance / 20 )).chanceOfNot { // Development prob: 0 at 0, 0.5 at 20, 0.9 at 180
+            return emptyProcessResult
+        }
+
+        group.cultureCenter.addNeedAspect(need.second)
+        group.populationCenter.wakeNeedStrata(need)
+
+        return emptyProcessResult
+    }
+
+    override val internalToString: String
+        get() = "Check resource needs"
+}
 
 object ForgetUnusedAspectsB : AbstractGroupBehaviour() {
     override fun run(group: Group): ProcessResult {
