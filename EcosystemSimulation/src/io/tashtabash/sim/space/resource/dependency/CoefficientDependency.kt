38a0cd6667794a071f1cb8cf7c701f52d8e2feb9
ShMPMat <matveyshnytkin@gmail.com>
2026-09-20 08:27:04 +0400

    Add CoefficientDependency comment

--- EcosystemSimulation/src/io/tashtabash/sim/space/resource/dependency/CoefficientDependency.kt
+++ EcosystemSimulation/src/io/tashtabash/sim/space/resource/dependency/CoefficientDependency.kt
@@ -4,7 +4,8 @@ import io.tashtabash.sim.space.resource.Resource
 import io.tashtabash.sim.space.tile.Tile
 
 
-abstract class CoefficientDependency(private val deprivationCoefficient: Double) : ResourceDependency {
+// Lower deprivation coefficients ease the severity of the dependency not being satisfied
+abstract class CoefficientDependency(val deprivationCoefficient: Double) : ResourceDependency {
     override fun satisfactionPercent(tile: Tile, resource: Resource, isSafe: Boolean): Double {
         val result = satisfaction(tile, resource, isSafe)
 
